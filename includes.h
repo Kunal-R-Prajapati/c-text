3aefc02e3d9b19d8bdc5e3f6c5f8a14f28025489
kkunalprajapati <prajapatikunal392@gmail.com>
2025-06-21 21:53:11 +0530

    includes .h file

---
+++ includes.h
@@ -0,0 +1,10 @@
+
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <string.h>
+#include <sys/types.h>
